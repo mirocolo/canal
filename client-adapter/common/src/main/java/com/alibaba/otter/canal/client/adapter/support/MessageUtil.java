91fe26a367bbb8482c086c4b2a6421535247e8c4
EtrnalGalaxy <13825920392@163.com>
2019-09-11 10:22:56 +0800

    兼容Msql BinLogMinimal行模式下无法更新的问题

--- client-adapter/common/src/main/java/com/alibaba/otter/canal/client/adapter/support/MessageUtil.java
+++ client-adapter/common/src/main/java/com/alibaba/otter/canal/client/adapter/support/MessageUtil.java
@@ -1,10 +1,20 @@
 package com.alibaba.otter.canal.client.adapter.support;
 
-import java.util.*;
-
 import com.alibaba.otter.canal.protocol.CanalEntry;
 import com.alibaba.otter.canal.protocol.FlatMessage;
 import com.alibaba.otter.canal.protocol.Message;
+
+import org.apache.commons.lang.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * Message对象解析工具类
@@ -14,188 +24,213 @@ import com.alibaba.otter.canal.protocol.Message;
  */
 public class MessageUtil {
 
-    public static List<Dml> parse4Dml(String destination, String groupId, Message message) {
-        if (message == null) {
-            return null;
-        }
-        List<CanalEntry.Entry> entries = message.getEntries();
-        List<Dml> dmls = new ArrayList<Dml>(entries.size());
-        for (CanalEntry.Entry entry : entries) {
-            if (entry.getEntryType() == CanalEntry.EntryType.TRANSACTIONBEGIN
-                || entry.getEntryType() == CanalEntry.EntryType.TRANSACTIONEND) {
-                continue;
-            }
+	private static final Logger logger = LoggerFactory.getLogger(MessageUtil.class);
 
-            CanalEntry.RowChange rowChange;
-            try {
-                rowChange = CanalEntry.RowChange.parseFrom(entry.getStoreValue());
-            } catch (Exception e) {
-                throw new RuntimeException("ERROR ## parser of eromanga-event has an error , data:" + entry.toString(),
-                    e);
-            }
+	public static List<Dml> parse4Dml(String destination, String groupId, Message message) {
+		if (message == null) {
+			return null;
+		}
+		List<CanalEntry.Entry> entries = message.getEntries();
+		List<Dml> dmls = new ArrayList<Dml>(entries.size());
+		for (CanalEntry.Entry entry : entries) {
+			if (entry.getEntryType() == CanalEntry.EntryType.TRANSACTIONBEGIN
+					|| entry.getEntryType() == CanalEntry.EntryType.TRANSACTIONEND) {
+				continue;
+			}
 
-            CanalEntry.EventType eventType = rowChange.getEventType();
+			CanalEntry.RowChange rowChange;
+			try {
+				rowChange = CanalEntry.RowChange.parseFrom(entry.getStoreValue());
+			} catch (Exception e) {
+				throw new RuntimeException("ERROR ## parser of eromanga-event has an error , data:" + entry.toString(),
+						e);
+			}
+//			logger.info("row:{}", rowChange.toString());
+			CanalEntry.EventType eventType = rowChange.getEventType();
 
-            final Dml dml = new Dml();
-            dml.setIsDdl(rowChange.getIsDdl());
-            dml.setDestination(destination);
-            dml.setGroupId(groupId);
-            dml.setDatabase(entry.getHeader().getSchemaName());
-            dml.setTable(entry.getHeader().getTableName());
-            dml.setType(eventType.toString());
-            dml.setEs(entry.getHeader().getExecuteTime());
-            dml.setIsDdl(rowChange.getIsDdl());
-            dml.setTs(System.currentTimeMillis());
-            dml.setSql(rowChange.getSql());
-            dmls.add(dml);
-            List<Map<String, Object>> data = new ArrayList<>();
-            List<Map<String, Object>> old = new ArrayList<>();
+			final Dml dml = new Dml();
+			dml.setIsDdl(rowChange.getIsDdl());
+			dml.setDestination(destination);
+			dml.setGroupId(groupId);
+			dml.setDatabase(entry.getHeader().getSchemaName());
+			dml.setTable(entry.getHeader().getTableName());
+			dml.setType(eventType.toString());
+			dml.setEs(entry.getHeader().getExecuteTime());
+			dml.setIsDdl(rowChange.getIsDdl());
+			dml.setTs(System.currentTimeMillis());
+			dml.setSql(rowChange.getSql());
+			dmls.add(dml);
+			List<Map<String, Object>> data = new ArrayList<>();
+			List<Map<String, Object>> old = new ArrayList<>();
 
-            if (!rowChange.getIsDdl()) {
-                Set<String> updateSet = new HashSet<>();
-                dml.setPkNames(new ArrayList<>());
-                int i = 0;
-                for (CanalEntry.RowData rowData : rowChange.getRowDatasList()) {
-                    if (eventType != CanalEntry.EventType.INSERT && eventType != CanalEntry.EventType.UPDATE
-                        && eventType != CanalEntry.EventType.DELETE) {
-                        continue;
-                    }
+			if (!rowChange.getIsDdl()) {
+				Set<String> updateSet = new HashSet<>();
+				dml.setPkNames(new ArrayList<>());
+				int i = 0;
+				for (CanalEntry.RowData rowData : rowChange.getRowDatasList()) {
+					if (eventType != CanalEntry.EventType.INSERT && eventType != CanalEntry.EventType.UPDATE
+							&& eventType != CanalEntry.EventType.DELETE) {
+						continue;
+					}
 
-                    Map<String, Object> row = new LinkedHashMap<>();
-                    List<CanalEntry.Column> columns;
+					Map<String, Object> row = new LinkedHashMap<>();
+					List<CanalEntry.Column> columns;
 
-                    if (eventType == CanalEntry.EventType.DELETE) {
-                        columns = rowData.getBeforeColumnsList();
-                    } else {
-                        columns = rowData.getAfterColumnsList();
-                    }
+					if (eventType == CanalEntry.EventType.DELETE) {
+						columns = rowData.getBeforeColumnsList();
+					} else if (eventType == CanalEntry.EventType.UPDATE) {
+						columns = rowData.getAfterColumnsList();
+						List<CanalEntry.Column> idColumnList = rowData.getBeforeColumnsList();
+						idColumnList = idColumnList.stream().filter(s -> StringUtils.contains(s.getName(), "id")).collect(Collectors.toList());
+						if (idColumnList.size() > 0) {
+							columns = new ArrayList<>(columns);
+							columns.addAll(idColumnList);
+						}
 
-                    for (CanalEntry.Column column : columns) {
-                        if (i == 0) {
-                            if (column.getIsKey()) {
-                                dml.getPkNames().add(column.getName());
-                            }
-                        }
-                        if (column.getIsNull()) {
-                            row.put(column.getName(), null);
-                        } else {
-                            row.put(column.getName(),
-                                JdbcTypeUtil.typeConvert(dml.getTable(),
-                                    column.getName(),
-                                    column.getValue(),
-                                    column.getSqlType(),
-                                    column.getMysqlType()));
-                        }
-                        // 获取update为true的字段
-                        if (column.getUpdated()) {
-                            updateSet.add(column.getName());
-                        }
-                    }
-                    if (!row.isEmpty()) {
-                        data.add(row);
-                    }
+					} else {
+						columns = rowData.getAfterColumnsList();
+					}
 
-                    if (eventType == CanalEntry.EventType.UPDATE) {
-                        Map<String, Object> rowOld = new LinkedHashMap<>();
-                        for (CanalEntry.Column column : rowData.getBeforeColumnsList()) {
-                            if (updateSet.contains(column.getName())) {
-                                if (column.getIsNull()) {
-                                    rowOld.put(column.getName(), null);
-                                } else {
-                                    rowOld.put(column.getName(),
-                                        JdbcTypeUtil.typeConvert(dml.getTable(),
-                                            column.getName(),
-                                            column.getValue(),
-                                            column.getSqlType(),
-                                            column.getMysqlType()));
-                                }
-                            }
-                        }
-                        // update操作将记录修改前的值
-                        if (!rowOld.isEmpty()) {
-                            old.add(rowOld);
-                        }
-                    }
+					for (CanalEntry.Column column : columns) {
+						if (i == 0) {
+							if (column.getIsKey()) {
+								dml.getPkNames().add(column.getName());
+							}
+						}
+						if (column.getIsNull()) {
+							row.put(column.getName(), null);
+						} else {
+							row.put(column.getName(),
+									JdbcTypeUtil.typeConvert(dml.getTable(),
+											column.getName(),
+											column.getValue(),
+											column.getSqlType(),
+											column.getMysqlType()));
+						}
+						// 获取update为true的字段
+						if (column.getUpdated()) {
+							updateSet.add(column.getName());
+						}
+					}
+					if (!row.isEmpty()) {
+						data.add(row);
+					}
 
-                    i++;
-                }
-                if (!data.isEmpty()) {
-                    dml.setData(data);
-                }
-                if (!old.isEmpty()) {
-                    dml.setOld(old);
-                }
-            }
-        }
+					if (eventType == CanalEntry.EventType.UPDATE) {
+						Map<String, Object> rowOld = new LinkedHashMap<>();
+						for (CanalEntry.Column column : rowData.getBeforeColumnsList()) {
+							if (updateSet.contains(column.getName())) {
+								if (column.getIsNull()) {
+									rowOld.put(column.getName(), null);
+								} else {
+									rowOld.put(column.getName(),
+											JdbcTypeUtil.typeConvert(dml.getTable(),
+													column.getName(),
+													column.getValue(),
+													column.getSqlType(),
+													column.getMysqlType()));
+								}
+							}
+						}
+						if (rowOld.isEmpty()) {
+							for (CanalEntry.Column column : rowData.getAfterColumnsList()) {
+								if (updateSet.contains(column.getName())) {
+									if (!column.getIsNull() && column.getUpdated()) {
+										rowOld.put(column.getName(),
+												JdbcTypeUtil.typeConvert(dml.getTable(),
+														column.getName(),
+														null,
+														column.getSqlType(),
+														column.getMysqlType()));
+									}
+								}
+							}
+						}
+						// update操作将记录修改前的值
+						if (!rowOld.isEmpty()) {
+							old.add(rowOld);
+						}
+					}
 
-        return dmls;
-    }
+					i++;
+				}
+				if (!data.isEmpty()) {
+					dml.setData(data);
+				}
+				if (!old.isEmpty()) {
+					dml.setOld(old);
+				}
+			}
+		}
 
-    public static List<Dml> flatMessage2Dml(String destination, String groupId, List<FlatMessage> flatMessages) {
-        List<Dml> dmls = new ArrayList<Dml>(flatMessages.size());
-        for (FlatMessage flatMessage : flatMessages) {
-            Dml dml = flatMessage2Dml(destination, groupId, flatMessage);
-            if (dml != null) {
-                dmls.add(dml);
-            }
-        }
+		return dmls;
+	}
 
-        return dmls;
-    }
+	public static List<Dml> flatMessage2Dml(String destination, String groupId, List<FlatMessage> flatMessages) {
+		List<Dml> dmls = new ArrayList<Dml>(flatMessages.size());
+		for (FlatMessage flatMessage : flatMessages) {
+			Dml dml = flatMessage2Dml(destination, groupId, flatMessage);
+			if (dml != null) {
+				dmls.add(dml);
+			}
+		}
 
-    public static Dml flatMessage2Dml(String destination, String groupId, FlatMessage flatMessage) {
-        if (flatMessage == null) {
-            return null;
-        }
-        Dml dml = new Dml();
-        dml.setDestination(destination);
-        dml.setGroupId(groupId);
-        dml.setDatabase(flatMessage.getDatabase());
-        dml.setTable(flatMessage.getTable());
-        dml.setPkNames(flatMessage.getPkNames());
-        dml.setIsDdl(flatMessage.getIsDdl());
-        dml.setType(flatMessage.getType());
-        dml.setTs(flatMessage.getTs());
-        dml.setEs(flatMessage.getEs());
-        dml.setSql(flatMessage.getSql());
-        // if (flatMessage.getSqlType() == null || flatMessage.getMysqlType() == null) {
-        // throw new RuntimeException("SqlType or mysqlType is null");
-        // }
-        List<Map<String, String>> data = flatMessage.getData();
-        if (data != null) {
-            dml.setData(changeRows(dml.getTable(), data, flatMessage.getSqlType(), flatMessage.getMysqlType()));
-        }
-        List<Map<String, String>> old = flatMessage.getOld();
-        if (old != null) {
-            dml.setOld(changeRows(dml.getTable(), old, flatMessage.getSqlType(), flatMessage.getMysqlType()));
-        }
-        return dml;
-    }
+		return dmls;
+	}
 
-    private static List<Map<String, Object>> changeRows(String table, List<Map<String, String>> rows,
-                                                        Map<String, Integer> sqlTypes, Map<String, String> mysqlTypes) {
-        List<Map<String, Object>> result = new ArrayList<>();
-        for (Map<String, String> row : rows) {
-            Map<String, Object> resultRow = new LinkedHashMap<>();
-            for (Map.Entry<String, String> entry : row.entrySet()) {
-                String columnName = entry.getKey();
-                String columnValue = entry.getValue();
+	public static Dml flatMessage2Dml(String destination, String groupId, FlatMessage flatMessage) {
+		if (flatMessage == null) {
+			return null;
+		}
+		Dml dml = new Dml();
+		dml.setDestination(destination);
+		dml.setGroupId(groupId);
+		dml.setDatabase(flatMessage.getDatabase());
+		dml.setTable(flatMessage.getTable());
+		dml.setPkNames(flatMessage.getPkNames());
+		dml.setIsDdl(flatMessage.getIsDdl());
+		dml.setType(flatMessage.getType());
+		dml.setTs(flatMessage.getTs());
+		dml.setEs(flatMessage.getEs());
+		dml.setSql(flatMessage.getSql());
+		// if (flatMessage.getSqlType() == null || flatMessage.getMysqlType() == null) {
+		// throw new RuntimeException("SqlType or mysqlType is null");
+		// }
+		List<Map<String, String>> data = flatMessage.getData();
+		if (data != null) {
+			dml.setData(changeRows(dml.getTable(), data, flatMessage.getSqlType(), flatMessage.getMysqlType()));
+		}
+		List<Map<String, String>> old = flatMessage.getOld();
+		if (old != null) {
+			dml.setOld(changeRows(dml.getTable(), old, flatMessage.getSqlType(), flatMessage.getMysqlType()));
+		}
+		return dml;
+	}
 
-                Integer sqlType = sqlTypes.get(columnName);
-                if (sqlType == null) {
-                    continue;
-                }
+	private static List<Map<String, Object>> changeRows(String table, List<Map<String, String>> rows,
+	                                                    Map<String, Integer> sqlTypes, Map<String, String> mysqlTypes) {
+		List<Map<String, Object>> result = new ArrayList<>();
+		for (Map<String, String> row : rows) {
+			Map<String, Object> resultRow = new LinkedHashMap<>();
+			for (Map.Entry<String, String> entry : row.entrySet()) {
+				String columnName = entry.getKey();
+				String columnValue = entry.getValue();
 
-                String mysqlType = mysqlTypes.get(columnName);
-                if (mysqlType == null) {
-                    continue;
-                }
+				Integer sqlType = sqlTypes.get(columnName);
+				if (sqlType == null) {
+					continue;
+				}
 
-                Object finalValue = JdbcTypeUtil.typeConvert(table, columnName, columnValue, sqlType, mysqlType);
-                resultRow.put(columnName, finalValue);
-            }
-            result.add(resultRow);
-        }
-        return result;
-    }
+				String mysqlType = mysqlTypes.get(columnName);
+				if (mysqlType == null) {
+					continue;
+				}
+
+				Object finalValue = JdbcTypeUtil.typeConvert(table, columnName, columnValue, sqlType, mysqlType);
+				resultRow.put(columnName, finalValue);
+			}
+			result.add(resultRow);
+		}
+		return result;
+	}
 }
